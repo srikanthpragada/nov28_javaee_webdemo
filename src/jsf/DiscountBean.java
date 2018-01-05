094da58a398852d93dd9272b12b2efa273671411
srikanthpragada <srikanthpragada@gmail.com>
2018-01-05 20:59:52 +0530

    5-JAN

--- src/jsf/DiscountBean.java
+++ src/jsf/DiscountBean.java
@@ -2,16 +2,23 @@ package jsf;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
 
 @ManagedBean
 public class DiscountBean {
+
 	private double amount, rate, discount;
+
+	public DiscountBean() {
+		System.out.println("DiscountBean()");
+	}
 
 	public double getAmount() {
 		return amount;
 	}
 
 	public void setAmount(double amount) {
+		System.out.println("setAmount()");
 		this.amount = amount;
 	}
 
@@ -27,9 +34,18 @@ public class DiscountBean {
 		return discount;
 	}
 
-	
 	public void calculate(ActionEvent evt) {
-		this.discount = this.amount * this.rate / 100; 
+		System.out.println("Calculate()");
+		this.discount = this.amount * this.rate / 100;
+	}
+
+	public void changed(ValueChangeEvent evt) {
+		System.out.println("Amount Changed  to :" + evt.getNewValue());
+
+	}
+
+	public void cancel(ActionEvent evt) {
+		System.out.println("cancel()");
 	}
 
 }
---
+++ src/jsf/JobsBean.java
@@ -0,0 +1,28 @@
+package jsf;
+
+import javax.faces.bean.ManagedBean;
+import javax.sql.RowSet;
+import javax.sql.rowset.CachedRowSet;
+
+import oracle.jdbc.rowset.OracleCachedRowSet;
+
+@ManagedBean
+public class JobsBean {
+	
+	public  RowSet getJobs() {
+	
+		try (CachedRowSet crs = new OracleCachedRowSet()) {
+			crs.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+			crs.setUsername("hr");
+			crs.setPassword("hr");
+			crs.setCommand("select * from jobs");
+			crs.execute();
+			return crs;
+		}
+		catch(Exception ex) {
+			return null;
+		}
+
+	}
+
+}
